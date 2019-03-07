46a379b7b92f2a7352145446f25f447965b2c257
rfelixmg <rfelixmg@gmail.com>
2019-03-07 11:22:40 +1030

    Update tester.sh
    changing epoch

--- src/sota/awa1/tester.sh
+++ src/sota/awa1/tester.sh
@@ -2,7 +2,7 @@
 _DBDIR_=./data/
 _DATABASE_=awa1
 _CONFERENCE_=sota
-_EPOCH_=epoch_80_80
+_EPOCH_=epoch_37_37
 _MODEL_=./experiments/$_CONFERENCE_/$_DATABASE_/cycle_wgan/classifier/checkpoint/$_EPOCH_/architecture.json
 
 python -m routines.tester -db $_DATABASE_ -rc 1 --load_model $_MODEL_  --dataroot $_DBDIR_
